a1bbc17c4ab9c5c16eaf1f1a256febdcd4b34242
Jordan Wesson <s201031@students.aie.edu.au>
2021-11-01 16:19:07 +1100

    added placeholder clock images, started CycleCharacterFrames to animate characters

---
+++ Project/Assets/Scripts/SophieScripts/CycleCharacterFrames.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CycleCharacterFrames : MonoBehaviour
+{
+    //public GameObject character;
+
+     class Character character;
+
+    public List<Sprite> frames;
+
+    public void Update()
+    {
+        int i = 0;
+
+
+    }
+}
