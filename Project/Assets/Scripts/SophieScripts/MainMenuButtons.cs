f6cbf1c9467d70d7d75d036a6a9b1624450900f6
Jordan Wesson <s201031@students.aie.edu.au>
2021-11-02 11:41:30 +1100

    put correct textures on menu buttons

---
+++ Project/Assets/Scripts/SophieScripts/MainMenuButtons.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    public void NewGameButton()
+    {
+
+    }
+
+    public void LoadGameButton()
+    { 
+
+    }
+
+    //public void 
+}
