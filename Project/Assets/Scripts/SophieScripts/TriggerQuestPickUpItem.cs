9350c36003dc2bb498d35cd0dec0e063638a8279
Jordan Wesson <s201031@students.aie.edu.au>
2021-11-02 15:42:15 +1100

    fixed Visual Studio InteliSense not working with Unity

---
+++ Project/Assets/Scripts/SophieScripts/TriggerQuestPickUpItem.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerQuestPickUpItem : MonoBehaviour
+{
+    public bool triggerQuestIsActive = false;
+    bool isTriggerQuestComplete = false;
+
+    //public GameObject pickUpItem = null;
+
+    void Update()
+    {
+        if (triggerQuestIsActive)
+        {
+            
+        }
+    }
+}
