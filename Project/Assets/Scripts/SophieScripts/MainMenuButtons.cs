c58326d4d37353e805d6be506e75ed112e0ccb75
Jordan Wesson <s201031@students.aie.edu.au>
2021-11-02 14:12:04 +1100

    Menu functionality, added MainMenuScene with menu canvas

--- Project/Assets/Scripts/SophieScripts/MainMenuButtons.cs
+++ Project/Assets/Scripts/SophieScripts/MainMenuButtons.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuButtons : MonoBehaviour
 {
     public void NewGameButton()
     {
+        Debug.Log("New Game");
 
+        string sceneName = "BaileyTestScene";
+
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
     }
 
     public void LoadGameButton()
-    { 
-
+    {
+        Debug.Log("Load Game");
     }
 
-    //public void 
+    public void SettingsButton()
+    {
+        Debug.Log("Settings");
+    }
+
+    public void CreditsButton()
+    {
+        Debug.Log("Credits");
+    }
+
+    public void ExitButton()
+    {
+        Debug.Log("Exit");
+
+        Application.Quit();
+    }
+
+    public void HelpButton()
+    {
+        Debug.Log("Help");
+    }
 }
