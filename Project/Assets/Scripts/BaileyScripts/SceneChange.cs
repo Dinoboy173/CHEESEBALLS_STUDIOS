8cac89deabc5e00dede997849f9ed6e646a399e0
Jordan Wesson <s201031@students.aie.edu.au>
2021-11-03 11:07:12 +1100

    got missing Beach and Mansion scenes and Change Scene script back

---
+++ Project/Assets/Scripts/BaileyScripts/SceneChange.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+
+    public void Cave()
+    {
+        SceneManager.LoadScene("CaveScene");
+    }
+
+    public void Mansion()
+    {
+        SceneManager.LoadScene("MansionScene");
+    }
+
+    public void Beach()
+    {
+        SceneManager.LoadScene("BeachScene");
+    }
+
+    public void Map()
+    {
+        SceneManager.LoadScene("MapScene");
+    }
+}
