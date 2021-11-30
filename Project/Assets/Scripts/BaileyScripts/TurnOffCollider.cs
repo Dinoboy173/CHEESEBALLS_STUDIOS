6f83b28eee7dabd7aa9309e4f49a10c6de4a5aa1
Bailey Kan <baik2704@gmail.com>
2021-12-01 10:39:43 +1100

    Intro Scene Updated to turn off mesh when paused

--- Project/Assets/Scripts/BaileyScripts/TurnOffCollider.cs
+++ Project/Assets/Scripts/BaileyScripts/TurnOffCollider.cs
@@ -8,6 +8,11 @@ public class TurnOffCollider : MonoBehaviour
     public GameObject hut; // or left cheese
     public GameObject mansion; // or middle cheese
     public GameObject cave; // or right cheese
+
+    public GameObject blue; // or left cheese
+    public GameObject chedder; // or middle cheese
+    public GameObject swiss; // or right cheese
+
     public GameObject pauseHandler;
     bool isPauseOn = false;
 
@@ -19,7 +24,9 @@ public class TurnOffCollider : MonoBehaviour
             {
                 ColliderOff();
                 isPauseOn = true;
+                break;
             }
+            isPauseOn = false;
         }
 
         if(isPauseOn == false)
@@ -31,23 +38,46 @@ public class TurnOffCollider : MonoBehaviour
 
     void ColliderOff()
     {
-       
-        Collider hutCollider = hut.GetComponent<SphereCollider>();
-        Collider mansionCollider = mansion.GetComponent<SphereCollider>();
-        Collider caveCollider = cave.GetComponent<SphereCollider>();
-        hutCollider.enabled = false;
-        mansionCollider.enabled = false;
-        caveCollider.enabled = false;
+        if (hut)
+        {
+            Collider hutCollider = hut.GetComponent<SphereCollider>();
+            Collider mansionCollider = mansion.GetComponent<SphereCollider>();
+            Collider caveCollider = cave.GetComponent<SphereCollider>();
+            hutCollider.enabled = false;
+            mansionCollider.enabled = false;
+            caveCollider.enabled = false;
+        }
+        else if(swiss)
+        {
+            Collider blueCollider= swiss.GetComponent<MeshCollider>();
+            Collider chedderCollider = blue.GetComponent<MeshCollider>();
+            Collider swissCollider = chedder.GetComponent<MeshCollider>();
+            blueCollider.enabled = false;
+            chedderCollider.enabled = false;
+            swissCollider.enabled = false;
+        }
     }
 
     void ColliderOn()
     {
+        if (hut)
+        {
+            Collider hutCollider = hut.GetComponent<SphereCollider>();
+            Collider mansionCollider = mansion.GetComponent<SphereCollider>();
+            Collider caveCollider = cave.GetComponent<SphereCollider>();
+            hutCollider.enabled = true;
+            mansionCollider.enabled = true;
+            caveCollider.enabled = true;
+        }
+        else if (swiss)
+        {
+            Collider blueCollider = swiss.GetComponent<MeshCollider>();
+            Collider chedderCollider = blue.GetComponent<MeshCollider>();
+            Collider swissCollider = chedder.GetComponent<MeshCollider>();
+            blueCollider.enabled = true;
+            chedderCollider.enabled = true;
+            swissCollider.enabled = true;
+        }
 
-        Collider hutCollider = hut.GetComponent<SphereCollider>();
-        Collider mansionCollider = mansion.GetComponent<SphereCollider>();
-        Collider caveCollider = cave.GetComponent<SphereCollider>();
-        hutCollider.enabled = true;
-        mansionCollider.enabled = true;
-        caveCollider.enabled = true;
     }
 }
